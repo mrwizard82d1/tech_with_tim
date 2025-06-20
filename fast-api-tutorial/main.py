5fb8edf531393331e60a8027f198ab9c4328ceae
Larry Jones <mrwizard82d1@gmail.com>
2025-06-19 20:51:10 -0500

    BROKE Seems like this change should work but no...
    I have more to learn.

--- fast-api-tutorial/main.py
+++ fast-api-tutorial/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,6 +14,10 @@ inventory = {
 }
 
 
-@app.get("/get-item/{item_id}")
-def get_item(item_id: int):
-    return inventory[item_id]
+# This is a silly example but illustrates multiple path components
+@app.get("/get-item/{item_id}/{brand}")
+def get_item_with_brand(item_id: int, brand: Optional[str] ):
+    if brand:
+        return inventory[item_id][brand]
+    else:
+        return inventory[item_id]
