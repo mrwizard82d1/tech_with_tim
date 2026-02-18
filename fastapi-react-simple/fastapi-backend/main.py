15dcf83d542089cd47de43d73db23a027e396de5
Larry Jones <mrwizard82d1@gmail.com>
2026-02-18 12:27:43 -0600

    Simple `main()` to respond to API requests
    - GET "/fruits"
    - POST "/fruits" with JSON describing name of fruit

--- fastapi-react-simple/fastapi-backend/main.py
+++ fastapi-react-simple/fastapi-backend/main.py
@@ -1,13 +1,48 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+
+class Fruit(BaseModel):
+    name: str
+
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    # To change, list allowed methods
+    allow_methods=["*"],
+    # Allows all headers (e.g. Authorization, Content-Type, etc.)
+    allow_headers=["*"],
+)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+memory_db = {'fruits': []}
+
+@app.get('/fruits', response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits=memory_db['fruits'])
+
+
+@app.post('/fruits', response_model=Fruit)
+def add_fruit(fruit: Fruit):
+    memory_db['fruits'].append(fruit)
+    return fruit
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
